3ca17a2c80de390132bd99e80799cdeda5e515d9
Neptali <njpm28@gmail.com>
2021-09-10 01:26:03 -0400

    static files config

--- plerk/settings.py
+++ plerk/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = Path(BASE_DIR) / 'staticfiles'
+STATICFILES_DIRS = ((Path(BASE_DIR) / 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
